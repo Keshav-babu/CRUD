31681da0a1f997646606893462d01cc5178bc704
Keshav-babu <keshavbabu742@gmail.com>
2022-03-25 09:45:50 +0530

    CRUD app in MERN stack

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express=require("express")
+const notes = require("./data/notes")
+const dotenv=require("dotenv")
+const app=express()
+
+app.get("/",(req,res)=>{
+    res.send("API is runnug")
+})
+
+app.get("/api/notes",(req,res)=>{
+    res.json(notes)
+})
+
+// app.get("api/notes/id",(req,res)=>{
+//     const note=notes.find((n)=>n._id===req.params.id)
+//     console.log(req.params)
+//     res.send(note)
+// })
+app.get("/api/notes/:id",(req,res)=>{
+    const note=notes.find((n)=>n._id===req.params.id)
+    console.log(note)
+    res.send(note)
+})
+
+
+const PORT=process.env.PORT || 5000
+app.listen(PORT,console.log(`Server started on PORT ${PORT}`))
